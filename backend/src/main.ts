a1101f225c5c7bd34ec7a8f61fc6d68231dbe9cb
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-12-03 08:57:05 -0300

    feat(backend): add libs prisma and swagger and make initial configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+  }));
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('GDASH Challenge API')
+    .setDescription('API para gerenciamento de dados climáticos e usuários')
+    .setVersion('1.0')
+    .addTag('weather')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
